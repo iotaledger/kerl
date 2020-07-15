7a7ac18a53e011622df948002a1018e98a843d43
Soatok Dreamseeker <soatok.dhole@gmail.com>
2020-07-14 23:46:46 -0400

    Increase readability of test code
    Since we only have one "expected" value (because of collisions in Kerl), we can simplify our definition.

--- javascript/test/collide.js
+++ javascript/test/collide.js
@@ -4,30 +4,24 @@ var Converter = require('../converter');
 var Kerl = require('../kerl');
 
 describe('kerlission', function() {
-    var tests = [{
-        input: 'GYOMKVTSNHVJNCNFBBAH9AAMXLPLLLROQY99QN9DLSJUHDPBLCFFAIQXZA9BKMBJCYSFHFPXAHDWZFEIZ',
-        expected: 'OXJCNFHUNAHWDLKKPELTBFUCVW9KLXKOGWERKTJXQMXTKFKNWNNXYD9DMJJABSEIONOSJTTEVKVDQEWTW'
-    },{
-        input: 'GYOMKVTSNHVJNCNFBBAH9AAMXLPLLLROQY99QN9DLSJUHDPBLCFFAIQXZA9BKMBJCYSFHFPXAHDWZFEIH',
-        expected: 'OXJCNFHUNAHWDLKKPELTBFUCVW9KLXKOGWERKTJXQMXTKFKNWNNXYD9DMJJABSEIONOSJTTEVKVDQEWTW'
-    },{
-        input: 'GYOMKVTSNHVJNCNFBBAH9AAMXLPLLLROQY99QN9DLSJUHDPBLCFFAIQXZA9BKMBJCYSFHFPXAHDWZFEIQ',
-        expected: 'OXJCNFHUNAHWDLKKPELTBFUCVW9KLXKOGWERKTJXQMXTKFKNWNNXYD9DMJJABSEIONOSJTTEVKVDQEWTW'
-    }];
+    var expected = 'OXJCNFHUNAHWDLKKPELTBFUCVW9KLXKOGWERKTJXQMXTKFKNWNNXYD9DMJJABSEIONOSJTTEVKVDQEWTW';
+    var tests = [
+        'GYOMKVTSNHVJNCNFBBAH9AAMXLPLLLROQY99QN9DLSJUHDPBLCFFAIQXZA9BKMBJCYSFHFPXAHDWZFEIZ',
+        'GYOMKVTSNHVJNCNFBBAH9AAMXLPLLLROQY99QN9DLSJUHDPBLCFFAIQXZA9BKMBJCYSFHFPXAHDWZFEIH',
+        'GYOMKVTSNHVJNCNFBBAH9AAMXLPLLLROQY99QN9DLSJUHDPBLCFFAIQXZA9BKMBJCYSFHFPXAHDWZFEIQ'
+    ];
 
     tests.forEach(function(test){
 
-        it('Should collide: ' + test.expected, function() {
-            var trits = Converter.trits(test.input);
+        it('Should collide: ' + expected, function() {
+            var trits = Converter.trits(test);
             var kerl = new Kerl();
             kerl.initialize();
             kerl.absorb(trits, 0, trits.length);
             var hashTrits = [];
             kerl.squeeze(hashTrits, 0, kerl.HASH_LENGTH);
             var hash = Converter.trytes(hashTrits);
-            assert.deepEqual(test.expected, hash);
+            assert.deepEqual(expected, hash);
         });
-
     });
-
 });
